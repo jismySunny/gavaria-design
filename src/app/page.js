9f1511b36254e7a2bd735b947067ba3d3036eddc
jismy12 <jismy.woodenclouds@gmail.com>
2024-05-17 11:01:33 +0530

    bug fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
 import HomeComponents from "@/components/maincomponents/homecomponents/HomeComponents";
 import PlayerComponents from "@/components/maincomponents/playercomponents/PlayerComponents";
 
 export default function Home({ Component, pageProps }) {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 280) setIsMobile(true);
-      else setIsMobile(false);
-    };
-
-    handleResize(); // Call it initially
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const Desktop = () => (
+  return (
     <>
       <HomeComponents />
       <PlayerComponents />
-    </>
-  );
-
-  return (
-    <>
-      {isMobile ? <DeviceNotSupport /> : <Desktop />}
     </>
   );
 }
